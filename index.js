371bbf295b9f5d17a6cebbea8978101511e2b54e
Abhishek <abhi.sankhwar22@gmail.com>
2025-05-04 16:59:31 +0530

    basic-update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = 'kehsihba321';
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const app = express();
 
